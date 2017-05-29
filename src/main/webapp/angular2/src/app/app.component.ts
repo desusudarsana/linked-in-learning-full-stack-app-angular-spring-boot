558c0e6f765f04f31482b5f1c12ffb6e145c0bdf
iamdesu <made.sudarsana@mitrais.com>
2017-05-29 22:59:05 +0800

    Create and style an Angular 2 component

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  public submitted: boolean;
+  roomsearch : FormGroup;
+
+  ngOnInit() {
+    this.roomsearch = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl('')
+    })
+  }
+
+  onSubmit({value, valid}:{value:RoomSearch, valid:boolean}) {
+    console.log(value);
+  }
+}
+
+export interface RoomSearch {
+  checkin:string;
+  checkout:string;
 }
